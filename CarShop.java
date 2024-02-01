f5f581e661541b95d9d4ba3517bdcb8d5969868f
ziggesommar <118897884+ziggesommar@users.noreply.github.com>
2024-02-01 12:35:02 +0100

    Update CarShop.java

--- CarShop.java
+++ CarShop.java
@@ -1,29 +1,14 @@
 public class CarShop <T extends Car>{
 
-
     protected Loadable <T> loadable;
-    //private Class<T> acceptedCarType;
-
 
     public CarShop(int capacity, double x, double y){
         this.loadable = new Loadable<>(capacity, x, y);
-        //this.acceptedCarType = null;
     }
-
-
-
-
-//    public CarShop(int capacity, double x, double y, Class<T> acceptedCarType){
-//        this.loadable = new Loadable<>(capacity, x, y);
-//        this.acceptedCarType = acceptedCarType;
-
-
-
-
+    
     public void load(T vehicle) {
         loadable.load(vehicle);
     }
-
 
     public T unload() {
         return loadable.unload();
